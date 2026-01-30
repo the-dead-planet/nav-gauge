8d6b009eac7ac88044464baff31e610676662d4a
spookydoodle <kxkaro@gmail.com>
2026-01-30 16:16:15 +0100

    Move test

---
+++ apps/packages/tinker-chest/test/example.test.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("Tinker chest", () => {
+    describe("Example test", () => {
+        it("should be equal 1", () => {
+            expect(1).to.be.equal(1);
+        });
+    });
+});
