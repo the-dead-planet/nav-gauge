f7d73a7904679c1c75d64393f91c46d82c757eec
spookydoodle <kxkaro@gmail.com>
2026-03-22 20:50:29 +0100

    update test

--- apps/app-mobile/__tests__/App.test.tsx
+++ apps/app-mobile/__tests__/App.test.tsx
@@ -1,16 +1,7 @@
 import { render } from '@testing-library/react-native';
 import { MobileMachineWard } from '../src/machine-ward';
 
-const machineWard = new MobileMachineWard({ 
-    "navigate": null,
-    "record-route": null,
-    "route-story": null,
-    "submit-data": null
-}, {
-    getItem: () => null,
-    removeItem: () => {},
-    setItem: (_key: string, _value: string) => {}
-}, false);
+const machineWard = new MobileMachineWard();
 
 test('basic test', () => {
     // render(<App />);
