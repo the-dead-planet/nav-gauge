c2ecd4bd2e68d9710f7e6612c8741c40d85aec98
spookydoodle <38886624+spookydoodle@users.noreply.github.com>
2026-04-27 08:48:15 +0200

    Chore/solve security vulnerabilities (#112)
    * security vulnerabilities add resolutions
    
    * solve ts err
    
    * zz

--- apps/packages/apparatus/src/state-warden/cartomancer/map-styles/background/background-map-style.ts
+++ apps/packages/apparatus/src/state-warden/cartomancer/map-styles/background/background-map-style.ts
@@ -1,7 +1,8 @@
+import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 import { MapStyle } from "../model";
 import backgroundStyle from "./background.json";
 
 export const backgroundMapStyle: MapStyle = {
     label: 'Background',
-    style: backgroundStyle as unknown as maplibregl.StyleSpecification
+    style: backgroundStyle as unknown as StyleSpecification
 };
--- apps/packages/apparatus/src/state-warden/cartomancer/map-styles/model.ts
+++ apps/packages/apparatus/src/state-warden/cartomancer/map-styles/model.ts
@@ -1,7 +1,8 @@
+import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 import { AttributionEntry } from "../../attribution-vault";
 
 export interface MapStyle {
     label: string;
-    style: string | maplibregl.StyleSpecification;
+    style: string | StyleSpecification;
     attribution?: AttributionEntry;
 }
