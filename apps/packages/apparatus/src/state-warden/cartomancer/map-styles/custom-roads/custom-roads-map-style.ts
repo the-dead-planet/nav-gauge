e807988927225dce133d4ca3186514e8205c5de8
spookydoodle <kxkaro@gmail.com>
2026-04-27 08:44:30 +0200

    solve ts err

--- apps/packages/apparatus/src/state-warden/cartomancer/map-styles/custom-roads/custom-roads-map-style.ts
+++ apps/packages/apparatus/src/state-warden/cartomancer/map-styles/custom-roads/custom-roads-map-style.ts
@@ -1,9 +1,10 @@
+import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 import { MapStyle } from "../model";
 import customRoadsStyle from "./custom-roads.json";
 
 export const customRoadsMapStyle: MapStyle = {
     label: 'Custom Paris roads (sample test)',
-    style: customRoadsStyle as unknown as maplibregl.StyleSpecification,
+    style: customRoadsStyle as unknown as StyleSpecification,
     attribution: {
         text: "Overture Maps",
         href: "https://docs.overturemaps.org/attribution"
--- apps/packages/apparatus/src/state-warden/cartomancer/map-styles/osm/osm-map-style.ts
+++ apps/packages/apparatus/src/state-warden/cartomancer/map-styles/osm/osm-map-style.ts
@@ -1,9 +1,10 @@
+import { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 import { MapStyle } from "../model";
 import osmStyle from "./osm.json";
 
 export const osmMapStyle: MapStyle = {
     label: 'OpenStreetMap',
-    style: osmStyle as unknown as maplibregl.StyleSpecification,
+    style: osmStyle as unknown as StyleSpecification,
     attribution: {
         text: "OpenStreetMap",
         href: "https://openstreetmap.org/copyright"
