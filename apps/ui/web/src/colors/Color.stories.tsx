4ab51b84598421083c0b6793d78894bb5119169f
spookydoodle <kxkaro@gmail.com>
2026-05-09 18:08:09 +0200

    add colors

--- apps/ui/web/src/colors/Color.stories.tsx
+++ apps/ui/web/src/colors/Color.stories.tsx
@@ -11,5 +11,23 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const ColorPalette = {
-  render: () => <Color color={Theme.palette.blue} />,
+  render: () => (
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: ' max-content max-content',
+      alignItems: 'center',
+      columnGap: '20px',
+    }}>
+      <Color name="Blue" color={Theme.palette.blue} />
+      <Color name="Copper" color={Theme.palette.copper} />
+      <Color name="Grey" color={Theme.palette.grey} />
+      <Color name="Lime" color={Theme.palette.lime} />
+      <Color name="Magenta" color={Theme.palette.magenta} />
+      <Color name="Pink" color={Theme.palette.pink} />
+      <Color name="Purple" color={Theme.palette.purple} />
+      <Color name="Red" color={Theme.palette.red} />
+      <Color name="Teal" color={Theme.palette.teal} />
+      <Color name="Yellow" color={Theme.palette.yellow} />
+    </div>
+  ),
 } satisfies Story;
--- apps/ui/web/src/colors/Color.tsx
+++ apps/ui/web/src/colors/Color.tsx
@@ -3,28 +3,35 @@ import { ColorShade, RGBColor, ThemeColor } from "@ui";
 import styles from './color.module.css';
 
 export interface ColorProps {
+    name: string;
     color: ThemeColor;
 }
 
-export const Color: FC<ColorProps> = ({ color }) => {
+export const Color: FC<ColorProps> = ({ name, color }) => {
     const data = (Object.entries(color) as unknown as [ColorShade, RGBColor][]);
     const reversed = data.toReversed();
 
     return (
-        <div className={styles.container}>
-            {data.map(([shade, c], i) => {
-                const textColor = reversed[i][1];
-                return (
-                    <p
-                        key={shade}
-                        className={styles.box}
-                        style={{
-                            color: `rgb(${textColor.r}, ${textColor.g}, ${textColor.b})`,
-                            backgroundColor: `rgb(${c.r}, ${c.g}, ${c.b})`
-                        }}
-                    >{shade}</p>
-                );
-            })}
-        </div>
+        <>
+            <h3>{name}</h3>
+            <div className={styles.palette}>
+                {data.map(([shade, c], i) => {
+                    const textColor = reversed[i][1];
+
+                    return (
+                        <p
+                            key={shade}
+                            className={styles.box}
+                            style={{
+                                color: `rgb(${textColor.r}, ${textColor.g}, ${textColor.b})`,
+                                backgroundColor: `rgb(${c.r}, ${c.g}, ${c.b})`
+                            }}
+                        >
+                            {shade}
+                        </p>
+                    );
+                })}
+            </div>
+        </>
     );
 };
