df9f20891967267e49a00e72bcc3d2c070219224
spookydoodle <kxkaro@gmail.com>
2026-05-10 13:25:23 +0200

    zz

--- apps/gears/route-story/web/src/hooks/useLoadedWebImages.tsx
+++ apps/gears/route-story/web/src/hooks/useLoadedWebImages.tsx
@@ -6,7 +6,7 @@ import { WebMarkerImageData } from "../images/image-parser";
  * @returns Filtered images with valid data and assignment to a feature id.
  */
 export const useLoadedWebImages = (images: MarkerImage<WebMarkerImageData>[]) => {
-    const loadedImages: LoadedImageData<WebMarkerImageData>[] = useMemo(() => images.filter(({ progress, error, ...image }) =>
+    const loadedImages: LoadedImageData<WebMarkerImageData>[] = useMemo(() => images.filter(({ progress, ...image }) =>
         progress === 100 && image.data?.bitmap && image.data.thumbnailBitmap && image.data.data && image.featureId !== undefined
     ) as LoadedImageData<WebMarkerImageData>[], [images]);
 
