6a155f9cafc132aa88c85a9f6f7f96aa6ae4d8d3
spookydoodle <kxkaro@gmail.com>
2026-08-02 09:13:28 +0200

    Remove footer content

--- app-mobile/src/layout/Footer.tsx
+++ app-mobile/src/layout/Footer.tsx
@@ -1,37 +1,16 @@
 import { FC } from "react";
-import { StyleSheet, View } from "react-native";
-import { useTheme } from "@ui";
-import { LinkText } from '@mobile-ui';
-import { useMachineWard } from "@apparatus";
-import { useSubjectState } from "@tinker-chest";
-
-const styles = StyleSheet.create({
-    container: {
-        height: 20,
-        boxSizing: 'content-box',
-        paddingHorizontal: 20,
-        paddingVertical: 4,
-        borderTopWidth: 1,
-        flexDirection: 'row',
-        display: 'flex',
-        justifyContent: 'flex-end'
-    }
-});
 
 export const Footer: FC = ({ }) => {
-    const theme = useTheme();
-    const { attributionVault } = useMachineWard();
-    const [attrributions] = useSubjectState(attributionVault.attributions$);
-
-    return (
-        <View style={[styles.container, {
-            borderColor: theme.componentColor('border')
-        }]}>
-            {[...attrributions.entries()].map(([id, { text, href }]) => (
-                <LinkText key={id} href={href}>
-                    © {text}
-                </LinkText>
-            ))}
-        </View>
-    );
+    return null; 
+    // return (
+    //     <View style={[styles.container, {
+    //         borderColor: theme.componentColor('border')
+    //     }]}>
+    //         {[...attrributions.entries()].map(([id, { text, href }]) => (
+    //             <LinkText key={id} href={href}>
+    //                 © {text}
+    //             </LinkText>
+    //         ))}
+    //     </View>
+    // );
 };
