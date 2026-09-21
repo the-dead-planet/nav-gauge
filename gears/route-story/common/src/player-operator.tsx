0d0586ded27babe5566028f034d4b442ec21df07
spookydoodle <kxkaro@gmail.com>
2026-09-21 07:25:43 +0200

    fix

--- gears/route-story/common/src/player-operator.tsx
+++ gears/route-story/common/src/player-operator.tsx
@@ -173,10 +173,7 @@ export class PlayerOperator<TMap, TChronoLens extends ChronoLens, TFile extends 
             last = now;
             routePlaybackFraction += dt / routePlaybackDuration;
             if (routePlaybackFraction >= 1) {
-<<<<<<< HEAD
-=======
                 this.gear.progressMs$.next(routeDuration);
->>>>>>> feature/204-playback-type
                 this.handleRouteEnd();
                 
                 return;
