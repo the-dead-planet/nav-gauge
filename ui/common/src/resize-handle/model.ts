3f4154da754deeb87ca10aee831bb1821b2e0df3
spookydoodle <kxkaro@gmail.com>
2026-09-23 09:23:04 +0200

    move tinkers

--- ui/common/src/resize-handle/model.ts
+++ ui/common/src/resize-handle/model.ts
@@ -9,16 +9,3 @@ export interface ResizeHandleProps {
     onDragEnd?: () => void;
     disabled?: boolean;
 }
-
-export const getResizeHandleGripPoints = (
-    direction: ResizeHandleProps['direction'],
-    side: NonNullable<ResizeHandleProps['side']>,
-): string => {
-    if (direction === 'horizontal') {
-        return side === 'right'
-            ? '4,0 6,0 8,2 8,22 6,24 4,24'
-            : '2,0 4,0 4,24 2,24 0,22 0,2';
-    }
-
-    return '2,0 22,0 24,2 24,4 0,4 0,2';
-};
---
+++ ui/common/src/resize-handle/tinkers.ts
@@ -0,0 +1,14 @@
+import { ResizeHandleProps } from './model';
+
+export const getResizeHandleGripPoints = (
+    direction: ResizeHandleProps['direction'],
+    side: NonNullable<ResizeHandleProps['side']>,
+): string => {
+    if (direction === 'horizontal') {
+        return side === 'right'
+            ? '4,0 6,0 8,2 8,22 6,24 4,24'
+            : '2,0 4,0 4,24 2,24 0,22 0,2';
+    }
+
+    return '2,0 22,0 24,2 24,4 0,4 0,2';
+};
